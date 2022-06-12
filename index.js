359ed9e140e55606444622379b9e39b2ba69827f
Ana Navarro <ana.elisacn.98@gmail.com>
2022-06-11 22:07:15 -0300

    Adding Register and Login Route

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
 
 const app = express();
 
 dotenv.config();
 
+const connect = async () => {
+    try{
+        mongoose.connect(process.env.DB);
+        console.log("DB is connected!");
+    } catch(err){
+        console.log(err)
+    }
+}
 app.listen(process.env.PORT || 3000 , () => {
     console.log(`Backend is running on ${process.env.PORT}`);
+    connect();
 });
 
+app.use(express.json());
 app.use("/api", authRoute)
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,67 @@ const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
-const validate = require("express-validator");
+const { check, validationResult } = require('express-validator');
+const auth = require("./middlewares/auth");
+const User = require("../models/User");
+const { generateToken } = require("./middlewares/auth")
 
 dotenv.config();
 
-router.use("/login", (req, res, next) => {});
-router.use("/register", (req, res, next) => {});
+router.post("/login",async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email })
+    if (user && (await user.matchPassword(password))) {
+            res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+            })
+        } else {
+            res.status(401)
+            throw new Error('Invalid email or password')
+        }
+
+    jwt.sign({ user }, process.env.JWT_SECRET, (err, token) => {
+        res.json({
+            token: token
+        });
+    });
+});
+
+router.post('/register', 
+    check('firstname', 'First Name is required').not().isEmpty(),
+    check('lastname', 'Last Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a valid password').isLength({min: 8})
+    , async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array() });
+        }
+        try{
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(req.body.password, salt);
+            const newUser = new User({
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email,
+                password: hash,
+                token: generateToken(newUser._id)
+            });
+            let userRegisted = await User.findOne({email});
+            if(userRegisted){
+                res.status(400).json({ erros: [{ msg:'User already exists' }]});
+            }
+            await newUser.save();
+            res.status(201).json(newUser);
+        }catch(err){
+            console.error(err);
+            res.status(500).send('Internal Error');
+        }
+});
+
+module.exports = router;
